e2c9224ceba9e2d379b514e11dbc884cc81769f5
Abyss_co <remi.dufeu@email.com>
2024-11-17 16:44:43 +0100

    db changes

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,15 @@
-// db.js
 const sqlite3 = require('sqlite3').verbose();
 
-// Connecter à la database
-const db = new sqlite3.Database('articles.db');
+// Connecter à la database avec lecture-écriture et création si elle n'existe pas
+const db = new sqlite3.Database('articles.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+    if (err) {
+        console.error("Erreur lors de l'ouverture de la base de données:", err.message);
+    } else {
+        console.log("Connexion réussie à la base de données.");
+    }
+});
 
-// Ajouter les elements
+// Ajouter les éléments
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS articles (
         id INTEGER PRIMARY KEY,
